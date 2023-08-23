cac217ae1f0960389bc8c53f22614bbfccc86030
Marcelo <marcelo.desouza@udesc.br>
2023-08-22 22:25:15 -0300

    Update material and src.

--- 3-src/data-structures/src/stack/TestStack.java
+++ 3-src/data-structures/src/stack/TestStack.java
@@ -4,25 +4,24 @@ import utils.Point;
 
 public class TestStack {
 	
-	//static ArrayStack<Point> stack = new ArrayStack<>(); 
-	static LinkedStack<Point> stack = new LinkedStack<>();
+	static Stack<Point> stack = new ArrayStack<>(); 
+	//static Stack<Point> stack = new LinkedStack<>();
 
 	public static void main(String[] args) {
-		System.out.println("Inserindo elementos na pilha...");
+		System.out.println("Empilhando quatro elementos...");
 		stack.push(new Point(1, -1));
 		stack.push(new Point(0, 6));
 		stack.push(new Point(9, 5));
 		stack.push(new Point(-3, 8));
 
-
 		System.out.println("Pilha: " + stack.toString());
-		System.out.println("Removendo um elemento: " + stack.pop().toString());
-		System.out.println("Removendo outro elemento: " + stack.top().toString());
+		System.out.println("Empilhando um elemento: " + stack.pop().toString());
+		System.out.println("Desempilhando outro elemento: " + stack.top().toString());
 		System.out.println("Pilha: " + stack.toString());
 		System.out.println("Tamanho: " + stack.size());
 		System.out.println("Vazia? " + stack.isEmpty());
 		
-		System.out.println("Removendo três elementos...");
+		System.out.println("Desempilhando três elementos...");
 		stack.pop();
 		stack.pop();
 		stack.pop();
